a507dfcfcf1712b050a0c7aa3dde38787760a391
driannns <saputraandrian064@gmail.com>
2024-03-21 23:18:37 +0700

    update endpoint

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ class DashboardController extends Controller
 {
     public function index(){
         $client = new Client();
-        $res = $client->get(config('API.API_URL'). "/?action=get_countries&APIkey=". config('API.API_KEY'));
+        $res = $client->get(config('app.API.API_URL'). "/?action=get_countries&APIkey=". config('app.API.API_KEY'));
         $resData = json_decode($res->getBody()->getContents());
 
         return view('dashboard',  compact('resData'));
@@ -18,14 +18,14 @@ class DashboardController extends Controller
     public function leagues(string $id){
         
         $client = new Client();
-        $res = $client->get(config('API.API_URL'). "/?action=get_leagues&country_id=". $id ."&APIkey=". config('API.API_KEY'));
+        $res = $client->get(config('app.API.API_URL'). "/?action=get_leagues&country_id=". $id ."&APIkey=". config('app.API.API_KEY'));
         $resData = json_decode($res->getBody()->getContents());
         return view('leagues',  compact('resData'));
     }
     
     public function teams(string $id){       
         $client = new Client();
-        $res = $client->get(config('API.API_URL'). "/?action=get_teams&league_id=". $id ."&APIkey=". config('API.API_KEY'));
+        $res = $client->get(config('app.API.API_URL'). "/?action=get_teams&league_id=". $id ."&APIkey=". config('app.API.API_KEY'));
         $resData = json_decode($res->getBody()->getContents());
     
         return view('teams',  compact('resData'));
